201299a3ee41e3d464e86f9f317fa80d1cb0d4c6
JoaoPedroso <146992416+JoaoPedroso520@users.noreply.github.com>
2026-04-28 19:17:26 -0300

    add health route

--- Front-End/js/main.js
+++ Front-End/js/main.js
@@ -1,5 +1,7 @@
 // Configuração da API
-const API_URL = 'http://localhost:5000/api';
+const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+    ? 'http://localhost:5000/api'
+    : 'https://c7-noticias-backend.onrender.com/api';
 let noticiaAtual = null;
 
 // Elementos do DOM
